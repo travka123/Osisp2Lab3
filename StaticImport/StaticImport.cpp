8b7f6528c5ac2c5585aa289488d40a67eb073c76
Киселёв Андрей <vkusnaya.travka@gmail.com>
2021-09-18 02:46:51 +0300

    static import done

---
+++ StaticImport/StaticImport.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#include "VirtualMemoryReplace.h"
+
+int main()
+{
+    SYSTEM_INFO sysInfo;
+    GetSystemInfo(&sysInfo);
+
+
+    char forFind[] = "Hello world";
+    char forReplace[] = "Hello user!";
+
+    char test1[] = "Hello world";
+    std::string test2 = "Hello world";
+    std::string test3 = " ((( Hello world )))";
+
+    std::cout << test1 << ' ' << test2 << ' ' << test3 << std::endl;
+
+    VirtualMemoryReplace(GetCurrentProcessId(), sysInfo.lpMinimumApplicationAddress, sysInfo.lpMaximumApplicationAddress, 
+        forFind, forReplace, strlen(forFind), strlen(forReplace));
+
+    std::cout << test1 << ' ' << test2 << ' ' << test3 << std::endl;
+
+    std::cin.get();
+}
+
+
